6a869fc7c2d5f34afa0440c2a7a8a4ebe4b96545
Rebeca Cruz <rebecacruz@me.com>
2020-05-11 18:11:28 -0300

    Classe PessoaService e metodo atualizar

--- src/main/java/com/example/algamoney/api/repository/PessoaRepository.java
+++ src/main/java/com/example/algamoney/api/repository/PessoaRepository.java
@@ -1,9 +1,12 @@
 package com.example.algamoney.api.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.algamoney.api.model.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
+
 
 }
