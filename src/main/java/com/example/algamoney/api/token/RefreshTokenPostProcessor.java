dcae8df38da7a8ce64a5ef3859dc76b1342473ff
Rebeca Cruz <rebecacruz@me.com>
2020-05-24 00:03:55 -0300

    Implementando o logout

--- src/main/java/com/example/algamoney/api/token/RefreshTokenPostProcessor.java
+++ src/main/java/com/example/algamoney/api/token/RefreshTokenPostProcessor.java
@@ -48,9 +48,7 @@ public class RefreshTokenPostProcessor implements ResponseBodyAdvice<OAuth2Acces
 	private void adicionarRefreshTokenNoCookie(String refreshToken, HttpServletRequest req, HttpServletResponse resp) {
 		Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
 		refreshTokenCookie.setHttpOnly(true);
-
-		refreshTokenCookie.setSecure(false);
-		
+		refreshTokenCookie.setSecure(false);		
 		refreshTokenCookie.setPath(req.getContextPath() + "/oauth/token");
 		refreshTokenCookie.setMaxAge(2592000);
 		
